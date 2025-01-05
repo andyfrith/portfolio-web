497860713006618a9d702936bf36f30e06227f54
Andy Frith <andy_frith@yahoo.com>
2025-01-05 12:46:03 -0700

    Early commit.  Adding new resume. Updating resume link in footer component.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="/AndyFrith-SeniorFrontendDeveloper.pdf"
+        href="/AndyFrith.pdf"
         target="_blank"
         rel="noopener noreferrer"
       >
